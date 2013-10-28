720c1406eb1ef745cc43873d91f315402f7b76a5
Hugh Rawlinson <hugh@codeoclock.net>
2013-10-28 02:32:00 +0000

    fixed player a bit more

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
+var audioelement = $('#radiostream').get(0);
 var togglePlayback = function(){
-	var audioelement = $('#radiostream').get(0);
 	!audioelement.paused?audioelement.pause():audioelement.play();
 	$($('#playPause').find('i')).attr('class',$('#radiostream').get(0).paused?'glyphicon glyphicon-play':'glyphicon glyphicon-pause');
 }
+
+$(audioelement).bind('canplay',function(){
+	$('loading').hide();
+})
